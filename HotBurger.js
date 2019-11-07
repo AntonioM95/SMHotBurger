0fdb7e556037b01031905d05705afc84d09f5490
Antonio Martinez <martinez_antonio19@yahoo.com>
2019-11-07 00:52:19 -0600

    logger file

--- HotBurger.js
+++ HotBurger.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const log = require('simple-node-logger').createSimpleFileLogger('project.log');
+const log = require('simple-node-logger').createSimpleFileLogger('Monitoring/monitoringlogs.log');
 const fs = require('fs');
 
 const menu = {
@@ -11,10 +11,16 @@ const menu = {
     Cookie: '$6'
 }
 
+
 logging = (arg) => {
     console.log(`Loggin results for ${arg}`)
     log.info(`Loggin results for ${arg}`);
 }
+
+logPurchase = (purchase) => {
+    log.info(`Purchase of :${purchase.item} :${purchase.quantity}`)
+}
+
 app.get('/version', (req, res) => {
  logging('/version');
  res.send('This is version 0 of the HotBurger service')
@@ -36,6 +42,11 @@ app.get('/getmenu', (req, res) => {
     res.send(menu);
 }
 );
+
+app.post('/purchase/:item/:quantity', (req, res) =>{
+    logPurchase(req.params);
+    res.send(200);
+});
 
 
 app.listen(port, () => console.log(`A new connection has been made`));
