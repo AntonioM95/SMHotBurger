60b43046871b6d5345ee5cfe67fecf4ee42d65eb
Antonio Martinez <martinez_antonio19@yahoo.com>
2019-11-07 22:35:38 -0600

    logs route

--- HotBurger.js
+++ HotBurger.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const log = require('simple-node-logger').createSimpleFileLogger('Monitoring/monitoringlogs.log');
+const log = require('simple-node-logger').createSimpleFileLogger('./Monitoring/monitoringlogs.log');
 const fs = require('fs');
 
 const menu = {
