6abeb76262766772d86e42747dae64afa3357b2a
Antonio Martinez <martinez_antonio19@yahoo.com>
2019-11-26 18:26:09 -0600

    new path

--- fakeOrderSerivice.js
+++ fakeOrderSerivice.js
@@ -9,7 +9,7 @@ const menu = {
 
 const purchase = (item, amount) =>{
     console.log("Purchasing");
-    request.post(`ec2-54-242-189-245.compute-1.amazonaws.com/purchase/${item}/${amount}`, {} , (err, res, body) => {
+    request.post(`ip-172-31-27-234.ec2.internal/purchase/${item}/${amount}`, {} , (err, res, body) => {
         if (err) { return console.log(err); }
         console.log(`statuscode: ${res.statusCode}`);
         console.log(body);
